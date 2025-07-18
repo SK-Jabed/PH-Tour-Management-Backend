f81a478fcd51d99f53a8bcd2ecbc5f6639a0fe87
Sheikh Jabed <jabedbd2295@gmail.com>
2025-07-18 23:58:29 +0600

    Activities updated on the server side

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import {Server} from "http";
 import mongoose from "mongoose";
 import app from "./app";
-import config from "./app/modules/config";
+// import config from "./app/modules/config";
+// import config from "./app/modules/config";
 
 let server: Server;
 
 const startServer = async () => {
     try {
-      await mongoose.connect(config.db_uri!);
+      await mongoose.connect(
+        "mongodb+srv://SKJ_69:Oi27ydF8DHFPXQJ3@cluster0.baizo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      );
 
       console.log("✅ Database Connected Successfully!");
 
-      app.listen(config.port, () => {
-        console.log(`✅ Server is running on port: ${config.port}`);
+      server = app.listen(5000, () => {
+        console.log(`✅ Server is running on port: 5000`);
       });
     } catch (error) {
       console.log(error);
